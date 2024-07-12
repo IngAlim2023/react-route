c417c2ed011f9f246b03b57be89c2da2fdc9d6cb
IngAlim2023 <enrique.porras@udea.edu.co>
2024-07-12 11:51:46 -0500

    Primer reto terminado

--- primerReact/src/pages/Home.jsx
+++ primerReact/src/pages/Home.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 const Home = () =>{
-    return <h1>Home</h1>
+    const [show, setshow] = useState(true);
+    
+return <div>
+        <h1>Home</h1>
+        <button onClick={()=>setshow(!show)}>Mostrar h3</button>
+        <h3 hidden={show}>Show</h3>
+    </div>
 }
 export default Home;
