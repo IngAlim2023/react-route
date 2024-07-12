a3fd85df293ec1f11b9bbf659b99775936b289d5
IngAlim2023 <enrique.porras@udea.edu.co>
2024-07-12 11:26:32 -0500

    Rutas construidas

--- primerReact/src/App.jsx
+++ primerReact/src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Default from "./pages/Default";
+import About from "./pages/About";
 
 function App() {
   
@@ -10,6 +13,9 @@ function App() {
       <h1>Rutas</h1>
       <Routes>
         <Route path='/' element={<Layout />}>
+        <Route path='/' element={<Home />}/>
+        <Route path='*' element={<Default />}/>
+        <Route path='/about' element={<About />}/>
         </Route>
       </Routes>      
     </div>
